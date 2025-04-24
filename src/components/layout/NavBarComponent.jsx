9190250d3c0b12fad24f6aac620f5ea0efb003dd
Arofath <suty2689@gmail.com>
2025-04-24 22:55:46 +0700

    fix: remove input in menu

--- src/components/layout/NavBarComponent.jsx
+++ src/components/layout/NavBarComponent.jsx
@@ -481,15 +481,6 @@ export default function NavbarComponents({
             ref={mobileMenuRef}
             className="fixed inset-0 top-14 sm:top-16 bg-white dark:bg-gray-950 z-40 flex flex-col"
           >
-            <div className="relative px-4 py-4 border-b border-gray-200 dark:border-gray-700">
-              <input
-                type="text"
-                placeholder="Search..."
-                className="w-full pl-10 pr-4 py-2 text-sm border rounded-full border-[#B9B28A] focus:outline-none focus:ring-2 focus:ring-gray-300 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:placeholder-gray-400"
-              />
-              <Search className="absolute left-8 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-500 dark:text-gray-400" />
-            </div>
-
             <div className="flex flex-col px-4 py-2 space-y-4">
               <NavLink
                 to="/"
